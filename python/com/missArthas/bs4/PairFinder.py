ba9d2fd91cee7da68f0274a02f79a213e988adaf
missArthas <kevin.huang@ximalaya.com>
2018-01-15 09:17:15 +0800

    updae the PairFinder,ignore the texts/

--- python/com/missArthas/bs4/PairFinder.py
+++ python/com/missArthas/bs4/PairFinder.py
@@ -10,6 +10,7 @@ class PairFinder(object):
     URL模式的底表
     字符集底表
     '''
+
     enFlagList = ['en', 'eng', 'english', 'en-us']
     cnFlagList = ['zh', 'zh-cn', 'ch', 'chi', 'sc', 'schi', 'han', 'utf-8']
 
@@ -23,8 +24,18 @@ class PairFinder(object):
     def __init__(self, path):
         self.basepath = path
 
-    def slashPathSearch(self, directory, web, enFlag, cnFlag):
-        savepath =  self.basepath + '/text/' + web
+    def slashPathSearch(self, readPath, savePath, enFlag, cnFlag):
+        """
+        :param readPath: /Users/nali/github/ParallelCorpus/websites/www.edb.edu.hk
+        :param savePath: /Users/nali/github/ParallelCorpus/texts/www.edb.edu.hk
+        :param enFlag:  /en/
+        :param cnFlag: /zh/
+        :return:
+        """
+        savepath =  savePath
+        if not os.path.exists(savepath):
+            os.mkdir(savepath)
+        directory = readPath
 
         result = []
         count = 0
@@ -51,21 +62,22 @@ class PairFinder(object):
                     #确保文件的编码是["utf-8", "gb2312", "gbk"]
                     with open(enpath, "rb") as f:
                         data = f.read()
-                        print(chardet.detect(data)['encoding'].lower())
-                        print("\n")
-                        if chardet.detect(data)['encoding'].lower() in self.encodingList:
-                            print("encoding is ok!\n")
-                            encodingFlag = True
-                        else:
-                            print("encoding is error!\n")
-                            encodingFlag = False
+                        if chardet.detect(data)['encoding'] != None:
+                            print(chardet.detect(data)['encoding'].lower())
+                            print("\n")
+                            if chardet.detect(data)['encoding'].lower() in self.encodingList:
+                                print("encoding is ok!\n")
+                                encodingFlag = True
+                            else:
+                                print("encoding is error!\n")
+                                encodingFlag = False
                     if encodingFlag == True:
                         count += 1
                         ensoup = BeautifulSoup(open(enpath), 'lxml')
                         cnsoup = BeautifulSoup(open(cnpath), 'lxml')
                         strs = html2Text.html2Text(ensoup, cnsoup)
 
-                        fileNum = web + '.' + str(count) + '.txt'
+                        fileNum = str(count) + '.txt'
                         print(fileNum)
 
                         if not os.path.exists(savepath + enFlag):
@@ -89,24 +101,37 @@ class PairFinder(object):
 
         return result
 
-    def slashSearchAll(self, directory):
-        #searchPath = self.basepath + '/websites/'
-        searchPath = self.basepath + directory
-        path = os.listdir(searchPath)
+    def slashSearchAll(self, readPath, savePath):
+        """
+        :param readPath: /Users/nali/github/ParallelCorpus/websites/
+        :param savePath: /Users/nali/github/ParallelCorpus/texts/
+        :return:
+        """
+        searchPath = readPath
+        webs= os.listdir(searchPath)
 
-        for dir in path:
+        for dir in webs:
             if os.path.isdir(searchPath + dir):  # 判断是否为文件夹，如果是输出所有文件就改成： os.path.isfile(p)
                 filepath = searchPath + dir
                 print(filepath)
 
                 pathDir = os.listdir(filepath)
-                for enFlag in self.enFlagList:
-                    for cnFlag in self.cnFlagList:
-                        self.slashPathSearch(filepath, dir, enFlag, cnFlag)
+                for enFlag in self.enSlashList:
+                    for cnFlag in self.cnSlashList:
+                        self.slashPathSearch(searchPath + dir, savePath + dir, enFlag, cnFlag)
 
-
-    def languagePathSearch(self, directory, web, enFlag, cnFlag):
-        savepath = self.basepath + web
+    def languagePathSearch(self, readPath, savePath, enFlag, cnFlag):
+        """
+        :param readPath: /Users/nali/github/ParallelCorpus/websites/www.edb.edu.hk
+        :param savePath: /Users/nali/github/ParallelCorpus/texts/www.edb.edu.hk
+        :param enFlag:  lang=en
+        :param cnFlag:  lang=cn
+        :return:
+        """
+        savepath =  savePath
+        if not os.path.exists(savepath):
+            os.mkdir(savepath)
+        directory = readPath
 
         result = []
         count = 0
@@ -116,7 +141,7 @@ class PairFinder(object):
 
         html2Text = Html2Text()
 
-        # 递归循环对目录下所有文件
+        #递归循环对目录下所有文件
         for root, dirs, files in os.walk(directory, topdown=True):
             for fl in files:
                 cnpath = root + "/" + fl
@@ -130,33 +155,34 @@ class PairFinder(object):
                     print(cnpath)
                     print("\n")
 
-                    # 确保文件的编码是["utf-8", "gb2312", "gbk"]
+                    #确保文件的编码是["utf-8", "gb2312", "gbk"]
                     with open(enpath, "rb") as f:
                         data = f.read()
-                        print(chardet.detect(data)['encoding'].lower())
-                        print("\n")
-                        if chardet.detect(data)['encoding'].lower() in self.encodingList:
-                            print("encoding is ok!\n")
-                            encodingFlag = True
-                        else:
-                            print("encoding is error!\n")
-                            encodingFlag = False
+                        if chardet.detect(data)['encoding'] != None:
+                            print(chardet.detect(data)['encoding'].lower())
+                            print("\n")
+                            if chardet.detect(data)['encoding'].lower() in self.encodingList:
+                                print("encoding is ok!\n")
+                                encodingFlag = True
+                            else:
+                                print("encoding is error!\n")
+                                encodingFlag = False
                     if encodingFlag == True:
                         count += 1
                         ensoup = BeautifulSoup(open(enpath), 'lxml')
                         cnsoup = BeautifulSoup(open(cnpath), 'lxml')
                         strs = html2Text.html2Text(ensoup, cnsoup)
 
-                        fileNum = web + '.' + str(count) + '.txt'
+                        fileNum = str(count) + '.txt'
                         print(fileNum)
 
-                        if not os.path.exists(savepath + enFlag):
-                            os.mkdir(savepath + enFlag)
-                        if not os.path.exists(savepath + cnFlag):
-                            os.mkdir(savepath + cnFlag)
+                        if not os.path.exists(savepath + "/" + enFlag + "/"):
+                            os.mkdir(savepath + "/" + enFlag)
+                        if not os.path.exists(savepath + "/" + cnFlag + "/"):
+                            os.mkdir(savepath + "/" + cnFlag)
 
-                        enfile = open(savepath + enFlag + fileNum, 'w+')
-                        cnfile = open(savepath + cnFlag + fileNum, 'w+')
+                        enfile = open(savepath + "/" + enFlag + "/" + fileNum, 'w+')
+                        cnfile = open(savepath + "/" + cnFlag + "/" + fileNum, 'w+')
 
                         enfile.write(strs[0])
                         enfile.close()
@@ -171,9 +197,45 @@ class PairFinder(object):
 
         return result
 
-    def languageSearchAll(self, directory):
-        ''
+    def languageSearchAll(self, readPath, savePath):
+        """
+        :param readPath: /Users/nali/github/ParallelCorpus/websites/
+        :param savePath: /Users/nali/github/ParallelCorpus/texts/
+        :return:
+        """
+        #searchPath = self.basepath + '/websites/'
+        paramList = ["language", "lang"]
+        searchPath = readPath
+        webs= os.listdir(searchPath)
+
+        for dir in webs:
+            if os.path.isdir(searchPath + dir):  # 判断是否为文件夹，如果是输出所有文件就改成： os.path.isfile(p)
+                filepath = searchPath + dir
+                print(filepath)
+
+                pathDir = os.listdir(filepath)
+                for enFlag in self.enFlagList:
+                    for cnFlag in self.cnFlagList:
+                        for language in paramList:
+                            self.languagePathSearch(searchPath + dir, savePath + dir, language + "=" + enFlag, language + "=" + cnFlag)
 
 
 pairFinder = PairFinder('/Users/nali/github/ParallelCorpus')
 print(pairFinder.basepath)
+# pairFinder.slashPathSearch('/Users/nali/github/ParallelCorpus/websites/www.edb.gov.hk',
+#                            '/Users/nali/github/ParallelCorpus/texts/www.edb.gov.hk',
+#                            '/en/', '/sc/')
+pairFinder.languageSearchAll('/Users/nali/github/ParallelCorpus/websites/', '/Users/nali/github/ParallelCorpus/texts/')
+
+# enpath = '/Users/nali/github/ParallelCorpus/websites/www.interface.com.cn/index.php?option=com_imagebank&view=downloads&id=1636&Itemid=109&lang=en&task=downloadzip'
+# encodingList = ["utf-8", "gb2312", "gbk"]
+# with open(enpath, "rb") as f:
+#     data = f.read()
+#     print(chardet.detect(data))
+#     print("\n")
+#     if chardet.detect(data)['encoding'].lower() in encodingList:
+#         print("encoding is ok!\n")
+#         encodingFlag = True
+#     else:
+#         print("encoding is error!\n")
+#         encodingFlag = False
